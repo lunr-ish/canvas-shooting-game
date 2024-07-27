95b19cd06a0a4dc70a8b91e011ee4eae346a78dc
lunr_ish <174506670+lunr-ish@users.noreply.github.com>
2024-07-27 23:16:38 +0530

    create enemies

--- index.js
+++ index.js
@@ -46,17 +46,61 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "blue");
 
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemy() {
+  setInterval(() => {
+    const x = 100;
+    const y = 100;
+    const radius = 30;
+    const color = "green";
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
   projectiles.forEach((projectile) => projectile.update());
+  enemies.forEach((enemy) => enemy.update());
   requestAnimationFrame(animate);
 }
 
@@ -77,3 +121,4 @@ addEventListener("click", (event) => {
 });
 
 animate();
+spawnEnemy();
