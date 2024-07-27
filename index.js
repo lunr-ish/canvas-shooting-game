1bbde5fcc7e0adeecf0c302034ccb595530dc486
lunr_ish <174506670+lunr-ish@users.noreply.github.com>
2024-07-27 22:43:05 +0530

    add projectiles on click

--- index.js
+++ index.js
@@ -22,9 +22,39 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "blue");
 
 player.draw();
+
+addEventListener("click", (event) => {
+  const projectile = new Projectile(
+    event.clientX,
+    event.clientY,
+    5,
+    "red",
+    null
+  );
+
+  projectile.draw();
+});
