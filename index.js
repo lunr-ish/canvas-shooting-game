fd1bdd6e37d8affcb52bcc098fcfa9416ea5cfcd
lunr_ish <174506670+lunr-ish@users.noreply.github.com>
2024-07-27 22:36:15 +0530

    create a player

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const canvas = document.querySelector("canvas");
 
+const ctx = canvas.getContext("2d");
+
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, "blue");
+
+player.draw();
