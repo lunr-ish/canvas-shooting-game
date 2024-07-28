f9cd80b93a85132a31cfc111e55ea0e9213a3aa4
lunr_ish <174506670+lunr-ish@users.noreply.github.com>
2024-07-28 23:45:08 +0530

    fade particles over time

--- index.js
+++ index.js
@@ -77,20 +77,25 @@ class Particle {
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
+    this.alpha = 1;
   }
 
   draw() {
+    ctx.save();
+    ctx.globalAlpha = this.alpha;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
+    ctx.restore();
   }
 
   update() {
     this.draw();
     this.x += this.velocity.x;
     this.y += this.velocity.y;
+    this.alpha -= 0.01;
   }
 }
 
@@ -138,7 +143,13 @@ function animate() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
 
-  particles.forEach((particle) => particle.update());
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+  });
 
   projectiles.forEach((projectile, index) => {
     projectile.update();
